e00681b479a1ce2c654bac7e94fc6780bcf0a385
MaksimS <sipatovGT@mail.ru>
2022-12-05 11:54:35 +0400

    FinalProjectFormatting

--- FinalProgect/Program.cs
+++ FinalProgect/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Написать программу, которая из имеющегося массива строк, формирует массив из строк, длина которых меньше либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
-// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 Console.WriteLine("Программа формирует массив строк из введённых Вами строк. Далее формирует новый массив, который содержит строки равные 3 или меньше символам!");
 
 int M = int.Parse(Console.ReadLine()!);
 Console.WriteLine($"Ваш размер массива {M}");
-string[] array = new string [M];
+string[] array = new string[M];
 
 GetArray(array);
 Console.WriteLine("Ваш массив:");
@@ -17,55 +17,55 @@ string[] newarray = GetNewArray(array);
 Console.WriteLine("Результат:");
 PrintNewArray(newarray);
 
-string[] GetArray(string[] array)                // Функция заполнения массива с консоли
+string[] GetArray(string[] array) // Функция заполнения массива с консоли
 {
- for (int i = 0; i < array.Length; i++) 
- { 
- array[i] = Console.ReadLine()!;
- }
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
 
- return array;
+    return array;
 }
 
-void PrintArray(string [] array)                 // функция вывода результата
+void PrintArray(string[] array) // функция вывода результата
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
 }
 
-int StringsLessThreeElements(string[] array)     // функция считает количество элементов в каждой строке из массива
+int StringsLessThreeElements(string[] array) // функция считает количество элементов в каждой строке из массива
 {
     int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length <= 3)
+    for (int i = 0; i < array.Length; i++)
     {
-    count++;
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
     }
-}
-return count;
+    return count;
 }
 
-string[] GetNewArray(string[] array)                // функция заполнения нового массива
+string[] GetNewArray(string[] array) // функция заполнения нового массива
 {
-    string[] newarray = new string [count];
+    string[] newarray = new string[count];
     int temp = 0;
     for (int i = 0; i < array.Length; i++)
     {
-    if (array[i].Length <= 3)
-    {
-        newarray[temp] = array[i];
-        temp++;
-    }
+        if (array[i].Length <= 3)
+        {
+            newarray[temp] = array[i];
+            temp++;
+        }
     }
     return newarray;
 }
 
-void PrintNewArray(string [] newarray)                 // функция вывода результата нового массива
+void PrintNewArray(string[] newarray) // функция вывода результата нового массива
 {
-    for(int i = 0; i < newarray.Length; i++)
+    for (int i = 0; i < newarray.Length; i++)
     {
         Console.Write(newarray[i] + " ");
     }
